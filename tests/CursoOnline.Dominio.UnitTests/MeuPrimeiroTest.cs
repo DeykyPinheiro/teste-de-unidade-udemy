a82f76c2c81ef5f6c58267a37fa90ea9488d9351
Deyky Pinheiro <pinheiromikael96@gmail.com>
2024-08-08 22:58:04 -0300

    Meu Primeiro Teste de unidade

---
+++ tests/CursoOnline.Dominio.UnitTests/MeuPrimeiroTest.cs
@@ -0,0 +1,14 @@
+﻿namespace CursoOnline.Dominio.UnitTests
+{
+    public class MeuPrimeiroTest
+    {
+        [Fact]
+        public void Testar()
+        {
+            var variavel1 = 1;
+            var variavel2 = 1;
+
+            Assert.Equal(variavel1, variavel2);
+        }
+    }
+}

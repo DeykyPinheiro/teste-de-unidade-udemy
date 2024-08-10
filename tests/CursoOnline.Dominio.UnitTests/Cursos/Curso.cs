e3b75b523d0205d9bc395b5fa0f3e4a40e863bca
Deyky Pinheiro <pinheiromikael96@gmail.com>
2024-08-10 13:14:44 -0300

    refatorando publico alvo para enum

--- tests/CursoOnline.Dominio.UnitTests/Cursos/Curso.cs
+++ tests/CursoOnline.Dominio.UnitTests/Cursos/Curso.cs
@@ -1,17 +1,19 @@
-﻿namespace CursoOnline.Dominio.UnitTests.Cursos
+﻿using static CursoOnline.Dominio.UnitTests.Cursos.CursoTest;
+
+namespace CursoOnline.Dominio.UnitTests.Cursos
 {
     public class Curso
     {
         public string Nome { get; private set; }
         public double CargaHoraria { get; private set; }
-        public string PublicoAlvo { get; private set; }
+        public PublicoAlvo PublicoAlvo { get; private set; }
         public double ValorCurso { get; private set; }
 
         //construtores vazio sempre privados para serem
         //usados apenas pela propria classe ou algum ORM
         private Curso() { }
 
-        public Curso(string nome, double cargaHoraria, string publicoAlvo, double valorCurso)
+        public Curso(string nome, double cargaHoraria, PublicoAlvo publicoAlvo, double valorCurso)
         {
             Nome = nome;
             CargaHoraria = cargaHoraria;
--- tests/CursoOnline.Dominio.UnitTests/Cursos/CursoTest.cs
+++ tests/CursoOnline.Dominio.UnitTests/Cursos/CursoTest.cs
@@ -10,7 +10,11 @@ namespace CursoOnline.Dominio.UnitTests.Cursos
 {
     public class CursoTest
     {
-
+        //admin, criar, editar curso para que sejam abertar matriculas para ele
+        //aceite:
+        //criar curso com: nome, carga horaria, publico alvo, valor do curso
+        //publico alvo: Estudante, Universitario, Empregado, Empreendedor
+        //todos os campos são obrigatorios
 
 
         [Fact]
@@ -21,7 +25,7 @@ namespace CursoOnline.Dominio.UnitTests.Cursos
             {
                 Nome = "Informatica básica",
                 CargaHoraria = (double)80,
-                PublicoAlvo = "Estudantes",
+                PublicoAlvo = PublicoAlvo.Estudante,
                 ValorCurso = (double)349.50
 
             };
@@ -31,6 +35,14 @@ namespace CursoOnline.Dominio.UnitTests.Cursos
 
             //Objeto esperado
             cursoEsperado.ToExpectedObject().ShouldMatch(curso);
+        }
+
+        public enum PublicoAlvo
+        {
+            Estudante,
+            Universitario,
+            Empregado,
+            Empreendedor
         }
     }
 }

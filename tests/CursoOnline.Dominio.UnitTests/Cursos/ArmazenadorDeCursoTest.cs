cc7a2d770dce0d836bfa5f64283499b3eed83969
Deyky Pinheiro <pinheiromikael96@gmail.com>
2024-08-10 17:34:31 -0300

    mockando classe de repository

---
+++ tests/CursoOnline.Dominio.UnitTests/Cursos/ArmazenadorDeCursoTest.cs
@@ -0,0 +1,56 @@
+﻿using Bogus.DataSets;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoOnline.Dominio.UnitTests.Cursos
+{
+    public class ArmazenadorDeCursoTest
+    {
+        [Fact]
+        public void DeveAdicionarCurso()
+        {
+            var cursoDto = new CursoDto
+            {
+                Nome = "Curso A",
+                CargaHoraria = 80,
+                PublicoAlvo = PublicoAlvo.Estudante,
+                ValorCurso = 300.10,
+                Descricao = "descricao"
+            };
+
+            var cursoRepositorioMock = new Mock<ICursoRepositorio>();
+
+            var armazenadorDeCurso = new ArmazenadorDeCurso(cursoRepositorioMock.Object);
+            armazenadorDeCurso.Armazenar(cursoDto);
+
+            cursoRepositorioMock.Verify(r => r.Adicionar(It.IsAny<Curso>()));
+
+        }
+    }
+
+    public interface ICursoRepositorio
+    {
+        Curso Adicionar(Curso data);
+    }
+
+    public class ArmazenadorDeCurso
+    {
+        private readonly ICursoRepositorio _cursoRepositorio;
+
+        public ArmazenadorDeCurso(ICursoRepositorio cursoRepositorio)
+        {
+            _cursoRepositorio = cursoRepositorio;
+        }
+
+        public Curso Armazenar(CursoDto data)
+        {
+            var curso = new Curso(data.Nome, data.Descricao, data.CargaHoraria, data.PublicoAlvo, data.ValorCurso);
+            var cursoAdicionado = _cursoRepositorio.Adicionar(curso);
+            return cursoAdicionado;
+        }
+    }
+}
---
+++ tests/CursoOnline.Dominio.UnitTests/Cursos/CursoDto.cs
@@ -0,0 +1,12 @@
+﻿
+namespace CursoOnline.Dominio.UnitTests.Cursos
+{
+    public class CursoDto
+    {
+        public string Nome { get; set; }
+        public int CargaHoraria { get; set; }
+        public PublicoAlvo PublicoAlvo { get; set; }
+        public double ValorCurso { get; set; }
+        public string Descricao { get; set; }
+    }
+}

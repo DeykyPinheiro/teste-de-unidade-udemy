096673702033269264414ad231dcd58fdd04abc7
Deyky Pinheiro <pinheiromikael96@gmail.com>
2024-08-10 13:04:48 -0300

    objetos esperados

--- tests/CursoOnline.Dominio.UnitTests/Cursos/CursoTest.cs
+++ tests/CursoOnline.Dominio.UnitTests/Cursos/CursoTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestPlatform.Common.Telemetry;
+﻿using ExpectedObjects;
+using Microsoft.VisualStudio.TestPlatform.Common.Telemetry;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,21 +10,27 @@ namespace CursoOnline.Dominio.UnitTests.Cursos
 {
     public class CursoTest
     {
-        private string nome = "Informatica básica";
-        private double cargaHoraria = 80;
-        private string publicoAlvo = "Estudantes";
-        private double valorCurso = 349.50;
+
+
 
         [Fact]
         public void DeveCriarCurso()
         {
-            Curso curso = new Curso(nome, cargaHoraria, publicoAlvo, valorCurso);
 
-            Assert.NotNull(curso);
-            Assert.Equal(nome, curso.Nome);
-            Assert.Equal(cargaHoraria, curso.CargaHoraria);
-            Assert.Equal(publicoAlvo, curso.PublicoAlvo);
-            Assert.Equal(valorCurso, curso.ValorCurso);
+            var cursoEsperado = new
+            {
+                Nome = "Informatica básica",
+                CargaHoraria = (double)80,
+                PublicoAlvo = "Estudantes",
+                ValorCurso = (double)349.50
+
+            };
+
+            Curso curso = new Curso(cursoEsperado.Nome, cursoEsperado.CargaHoraria,
+                cursoEsperado.PublicoAlvo, cursoEsperado.ValorCurso);
+
+            //Objeto esperado
+            cursoEsperado.ToExpectedObject().ShouldMatch(curso);
         }
     }
 }
